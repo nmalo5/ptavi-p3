7e18983e3e705a0e49d307291e2f8c0c8ca027d7
nmalo5 <nmszidane@msn.com>
2014-10-12 18:36:00 +0200

    COMPLETO+PEP8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,10 +4,11 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
-    
+    def __init__(self):
+
         self.width = ""
         self.heigth = ""
         self. background_color = ""
@@ -21,57 +22,51 @@ class SmallSMILHandler(ContentHandler):
         self.begin = ""
         self.dur = ""
         self.lista = []
-    def startElement(self, name, attrs):    
-     
+
+    def startElement(self, name, attrs):
+
         if name == "root-layout":
             self.width = attrs.get("width", "")
             self.height = attrs.get("height", "")
             self.background_color = attrs.get("background-color", "")
             self.lista = (self.lista + ["root-layout", "width", self.width,
-                        "heigth", self.height,"background-color", 
-                        self.background_color])
-           # print self.lista
+                          "heigth", self.height, "background-color",
+                          self.background_color])
         elif name == "region":
             self.id = attrs.get("id", "")
             self.top = attrs.get("top", "")
             self.bottom = attrs.get("bottom", "")
             self.left = attrs.get("left", "")
             self.rigth = attrs.get("rigth", "")
-            self.lista = (self.lista + ["region", "id", self.id, "top", 
-                        self.top, "bottom", self.bottom, "left", self.left,
-                        "rigth", self.rigth]) 
+            self.lista = (self.lista + ["region", "id", self.id, "top",
+                          self.top, "bottom", self.bottom, "left", self.left,
+                          "rigth", self.rigth])
         elif name == "img":
             self.src = attrs.get("src", "")
             self.region = attrs.get("region", "")
             self.begin = attrs.get("begin", "")
             self.dur = attrs.get("dur", "")
             self.lista = (self.lista + ["img", "src", self.src, "region",
-                        self.region, "begin", self.begin, "dur", self.dur])
+                          self.region, "begin", self.begin, "dur", self.dur])
         elif name == "audio":
             self.src = attrs.get("src", "")
             self.begin = attrs.get("begin", "")
             self.dur = attrs.get("dur", "")
             self.lista = (self.lista + ["audio", "src", self.src, "begin",
-                        self.begin,"dur", self.dur])
+                          self.begin, "dur", self.dur])
         elif name == "textstream":
             self.src = attrs.get("src", "")
             self.region = attrs.get("region", "")
-            self.lista = (self.lista + ["textstream", "src", self.src, 
-                        "region", self.region])
-            
-    
+            self.lista = (self.lista + ["textstream", "src", self.src,
+                          "region", self.region])
+
     def get_tags(self):
         return self.lista
-       
-        
 #if __name__ == "__main__":
-   
+
  #   parser = make_parser()
    # cHandler = SmallSMILHandler()
     #parser.setContentHandler(cHandler)
     #parser.parse(open('karaoke.smil'))
     #ejemplo = cHandler
     #print ejemplo.get_tags()
-  
-        
-        
