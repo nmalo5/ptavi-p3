9f3cf1bd3e863e15767ad4c7e387bb38386f5c84
nmalo5 <nmszidane@msn.com>
2014-10-12 18:53:28 +0200

    COMPLETOej4+PEP8

--- karaoke.py
+++ karaoke.py
@@ -13,7 +13,6 @@ try:
     parser.parse(open('karaoke.smil'))
     ejemplo = cHandler
     lista = ejemplo.get_tags()
-    posregion = 7  #guardamos la pos de la etiqueta region para diferenciar mas adelante
     hayblancos = "" in lista
     while hayblancos:
         borrarelem = (lista.index("") - 1)
@@ -22,45 +21,45 @@ try:
         hayblancos = "" in lista
     pos = []
     n = 0
-    for e in lista:   #Buscamos la posición de las etiquetas
-        if e == "root-layout" or e == "img" or e == "audio" or e =="textstream":
+    for e in lista:  # Buscamos la posición de las etiquetas
+        if (e == "root-layout" or e == "img" or e == "audio"
+           or e == "textstream"):
             pos.append(n)
-        elif e == "region": #En region debemos diferenciar entre etiqueta y atributo
+        elif e == "region":  # Para diferenciar entre la etiqueta y el atributo
             etq = lista[n+1]
-            if etq == "id" or etq == "top" or etq == "bottom" or etq == "left" or etq == "right":
+            if (etq == "id" or etq == "top" or etq == "bottom" or etq == "left"
+               or etq == "right"):
                 pos.append(n)
         n = n + 1
-    print len(lista)
     cadena = ""
-    n = 0 #recorre la lista lista 
+    n = 0  # Recorre la lista lista
     imprimir = False
     etiqueta = False
-    for i in lista: 
+    for i in lista:
         if n == len(lista)-1 and n != 0:
-           imprimir = True
-        for num in pos : 
-            if (num-1) == n and n!= 0:
+            imprimir = True
+        for num in pos:
+            if (num-1) == n and n != 0:
                 imprimir = True
-            elif num == n:     #para tratar a region como atributo o etiqueta
+            elif num == n:  # Para tratar a region como atributo o etiqueta
                 etiqueta = True
-        if (i == "width" or i == "heigth" or i == "background-color" or i == "id"
-            or i == "top" or i == "bottom" or i == "left" or i == "right" 
-            or i == "src" or i == "begin" or i == "dur"):  #todos los atributos excepto region
+        if (i == "width" or i == "heigth" or i == "background-color" or i ==
+           "id" or i == "top" or i == "bottom" or i == "left" or i == "right"
+           or i == "src" or i == "begin" or i == "dur"):  # Todos menos region
             cadena = cadena + "\t"
-            cadena = cadena + i 
+            cadena = cadena + i
             cadena = cadena + "="
         elif i == "region" and not etiqueta:
             cadena = cadena + "\t"
-            cadena = cadena + i 
+            cadena = cadena + i
             cadena = cadena + "="
         else:
             cadena = cadena + i
-            
-        
+
         if imprimir:
-            print cadena 
+            print cadena
             cadena = ""
-            imprimir = False   
+            imprimir = False
         etiqueta = False
         n = n + 1
 except IndexError:
